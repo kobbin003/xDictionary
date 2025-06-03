3fbc3583f47516e8dc68dabfef2b69b4a83ba01a
kobbin003 <dkfeto@gmail.com>
2025-06-03 13:07:12 +0530

    xDictionary done

---
+++ README.md
@@ -0,0 +1,5 @@
+# xDictionary
+
+A task from crio's sprint
+
+This is a simple React-based dictionary application that lets users search for the definition of predefined words.
---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./App.css";
+
+const dictionary = [
+	{
+		word: "React",
+		meaning: "A JavaScript library for building user interfaces.",
+	},
+
+	{ word: "Component", meaning: "A reusable building block in React." },
+
+	{ word: "State", meaning: "An object that stores data for a component." },
+];
+
+function App() {
+	const [word, setWord] = useState("");
+	const [definition, setDefinition] = useState("");
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const definition = dictionary.find((pair) => pair.word == word);
+		if (definition) {
+			setDefinition(definition["meaning"]);
+		} else {
+			setDefinition("Word not found in the dictionary.");
+		}
+	};
+
+	const handleInputChange = (e) => {
+		setWord(e.target.value);
+	};
+	return (
+		<div>
+			<h1>Dictionary App</h1>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					name="word"
+					id="word"
+					value={word}
+					onChange={handleInputChange}
+				/>
+				<button type="submit">Search</button>
+			</form>
+			<div>
+				<h3>Defintion:</h3>
+				<p>{definition}</p>
+			</div>
+		</div>
+	);
+}
+
+export default App;
